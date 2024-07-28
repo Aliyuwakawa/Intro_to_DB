d5a062bfc7ad4040cca61b7f3d942e072d963f8f
Aliyuwakawa <aliyuwakawa76@gmail.com>
2024-07-28 11:58:48 -0700

    b

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
 USE alx_book_store;
-SHOW CREATE TABLE Books;
+
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE,  
+    COLUMN_KEY,   
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store', 
+    TABLE_NAME = 'Books';
 
